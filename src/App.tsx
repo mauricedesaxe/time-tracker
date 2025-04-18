1c6038658e034e2e809a12a982accf75c753e822
lazaralex98 <alex2012ro@gmail.com>
2025-04-18 18:17:10 +0300

    add sidebar for future charts

--- src/App.tsx
+++ src/App.tsx
@@ -9,14 +9,20 @@ function App() {
   }, []);
 
   return (
-    <div className="min-h-screen py-6 flex flex-col justify-center sm:py-12">
-      <div className="relative py-3 sm:max-w-xl sm:mx-auto">
-        <div className="relative px-4 py-10 bg-white shadow-lg rounded-lg sm:rounded-3xl sm:p-8 min-w-xs">
-          <h1 className="text-2xl font-bold text-gray-800 mb-6">
-            Simple Time Tracker
-          </h1>
-          <TimeTracker />
-        </div>
+    <div className="flex min-h-full flex-col">
+      <div className="mx-auto flex w-full max-w-7xl items-start gap-x-8 px-4 py-10 sm:px-6 lg:px-8">
+        <main className="flex-1">
+          <div className="relative px-4 py-10 bg-white shadow-lg rounded-lg sm:rounded-3xl sm:p-8 min-w-xs">
+            <h1 className="text-2xl font-bold text-gray-800 mb-6">
+              Simple Time Tracker
+            </h1>
+            <TimeTracker />
+          </div>
+        </main>
+
+        <aside className="sticky top-8 hidden w-96 shrink-0 xl:block">
+          {/* Right column area */}
+        </aside>
       </div>
     </div>
   );
