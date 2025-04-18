e48a8eac572290b459bb312059145d6788cf183e
lazaralex98 <alex2012ro@gmail.com>
2025-04-18 17:18:08 +0300

    improve card style

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
   return (
     <div className="min-h-screen py-6 flex flex-col justify-center sm:py-12">
       <div className="relative py-3 sm:max-w-xl sm:mx-auto">
-        <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-8">
+        <div className="relative px-4 py-10 bg-white shadow-lg rounded-lg sm:rounded-3xl sm:p-8 min-w-xs">
           <h1 className="text-2xl font-bold text-gray-800 mb-6">
             Simple Time Tracker
           </h1>
